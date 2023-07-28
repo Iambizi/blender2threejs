88f26faca70e8d2c7991d4f23991bf7d85e1c617
Iambizi <amirbzmn@gmail.com>
2023-07-28 00:31:52 -0400

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useGLTF, OrbitControls, Environment } from '@react-three/drei';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Model = () => {
+    const gltf = useGLTF('/WIP.glb', false);
+    return <primitive object={gltf.scene} scale={0.5} />;
+};
+
+const App = () => {
+    return (
+        <Canvas style={{ display: "block", margin: "0 auto" }} camera={{ position: [0, 0, 2], fov: 40 }}>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Model />
+            <OrbitControls autoRotate />
+            <Environment preset="sunset" background />
+        </Canvas>
+    );
+};
 
 export default App;
+
+
